903567000917f547d29fa7093a14091787fe4870
Iain <iain@iainferguson.org>
2020-06-12 22:01:52 +0100

    changed setup to bootstrap 4

--- src/create_todo.js
+++ src/create_todo.js
@@ -1,5 +1,6 @@
 const todoModule = (() => {
   let list = [];
+  let id = 0;
 
   return {
     addTodoList: (title, descritpion, dueDate, priority, notes) => {
@@ -8,8 +9,10 @@ const todoModule = (() => {
       this.dueDate = dueDate;
       this.priority = priority;
       this.notes = notes;
+      this.entryId = id;
 
-      list.push({ title, descritpion, dueDate, priority, notes });
+      list.push({ title, descritpion, dueDate, priority, notes, id });
+      id++;
       return list;
     },
 
