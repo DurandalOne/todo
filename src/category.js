d3181eb777c08dfd2efec8b24b01bc91daa81344
Iain <iain@iainferguson.org>
2020-06-14 13:38:54 +0100

    tweaked category.js

--- src/category.js
+++ src/category.js
@@ -1,11 +1,18 @@
-const category = () => {
+const category = (() => {
   let currentCategory = [];
+  let categoryList = [];
 
   return {
     createCategory: function createCategory(name) {
       currentCategory.push(name);
     },
+
+    deleteCategory: function deleteCategory(name) {
+      let catIndex;
+    },
+
+    currentCategory: currentCategory,
   };
-};
+})();
 
 export { category };
--- src/create_todo.js
+++ src/create_todo.js
@@ -1,17 +1,20 @@
+import { category } from "./category";
+
 const todoModule = (() => {
   let list = [];
   let id = 0;
 
   return {
-    addTodoList: (title, descritpion, dueDate, priority, notes) => {
+    addTodoList: (title, descritpion, dueDate, priority, notes, category) => {
       this.title = title;
       this.descritpion = descritpion;
       this.dueDate = dueDate;
       this.priority = priority;
       this.notes = notes;
       this.entryId = id;
+      category.currentCategory = category;
 
-      list.push({ title, descritpion, dueDate, priority, notes, id });
+      list.push({ title, descritpion, dueDate, priority, notes, id, category });
       id++;
       return list;
     },
